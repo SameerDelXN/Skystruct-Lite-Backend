4ebbd2af160815edad1428bf5f3cd8079660b8c5
Sameer Gaikwad <sameergaikwad.delxn@gmail.com>
2025-11-08 15:31:03 +0530

    added project image

--- app/api/auth/verify-email/route.ts
+++ app/api/auth/verify-email/route.ts
@@ -1,28 +1,4 @@
-// import dbConnect from "@/lib/dbConnect";
-// import User from "@/models/User";
-// import { NextResponse } from "next/server";
 
-// export async function GET(req: Request) {
-//   await dbConnect();
-// //sample
-//   const { searchParams } = new URL(req.url);
-//   const token = searchParams.get("token");
-
-//   if (!token)
-//     return NextResponse.json({ success: false, message: "Missing verification token" }, { status: 400 });
-
-//   const user = await User.findOne({ emailVerificationToken: token });
-
-//   if (!user)
-//     return NextResponse.json({ success: false, message: "Invalid or expired token" }, { status: 400 });
-
-//   user.isEmailVerified = true;
-//   user.emailVerificationToken = null;
-//   await user.save();
-
-//   return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/verified`);
-// }
-//sample
 import dbConnect from "@/lib/dbConnect";
 import User from "@/models/User";
 import { NextResponse } from "next/server";
